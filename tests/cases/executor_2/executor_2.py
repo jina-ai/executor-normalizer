3988c838a1ce81f73d8b332d7d72019223b2e38d
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-09-09 09:17:26 +0100

    test: add args extraction unit tests

---
+++ tests/cases/executor_2/executor_2.py
@@ -0,0 +1,53 @@
+from typing import Optional, List, Tuple, Dict
+
+import jina
+from jina import Executor, requests, DocumentArray
+
+
+class Executor2(Executor):
+    """
+    My executor docstring
+    :param: arg1
+    :param: arg2
+    """
+    def __init__(
+            self,
+            arg1: Optional[
+                Tuple[
+                    int,
+                    str,
+                    List[int]
+                ]
+            ],
+            arg2: Optional[
+                Tuple[
+                    int,
+                    str,
+                    List[int]
+                ]
+            ] = (123, 'test', [123]),
+            **kwargs):
+        """
+        init docstring
+        """
+        super().__init__(**kwargs)
+
+    @requests(on=['/index', '/foo'])
+    def foo(
+            self,
+            arg1: Tuple[
+                int,
+                str
+            ],
+            arg2: Optional[int] = None,
+    ):
+        """foo docstring"""
+        pass
+
+    @requests(on='/bar')
+    def bar(self, docs: Optional[DocumentArray], parameters: Dict = {}):
+        """bar docstring
+        :param docs:
+        :param parameters:
+        """
+        pass
--- tests/test_core.py
+++ tests/test_core.py
@@ -14,6 +14,9 @@ def test_inspect_dummy_execs():
     assert executors[2][0] == 'Dummy3Executor'
     assert executors[3][0] == 'FailedExecutor'
 
+    for executor in executors:
+        assert len(executor) == 5
+
     # success case
     assert len(executors[0][1]) == 1
     assert len(executors[0][2]) == 0
@@ -26,6 +29,54 @@ def test_inspect_dummy_execs():
     assert len(executors[3][1]) == 2
     assert len(executors[3][2]) == 0
 
+
+@pytest.mark.parametrize('package_path, expected', [
+    (
+        Path(__file__).parent / 'cases' / 'executor_1',
+        ('Executor1', 'My executor docstring', (
+            [('self', None), ('arg1', 'str'), ('arg2', 'Optional[List[str]]'), ('arg3', 'Tuple[int, str]')],
+            [('arg4', 'Optional[int]', 'None'), ('arg5', 'Optional[Tuple[int, str]]', '(123, \'123\')')],
+            'init docstring'
+        ), [('foo', [('self', None)], [], 'foo docstring', 'ALL')])
+    ),
+    (
+        Path(__file__).parent / 'cases' / 'executor_2',
+        ('Executor2', 'My executor docstring\n:param: arg1\n:param: arg2', (
+            [('self', None), ('arg1', 'Optional[Tuple[int, str, List[int]]]')],
+            [('arg2', 'Optional[Tuple[int, str, List[int]]]', "(123, 'test', [123])")],
+            'init docstring'
+        ), [
+            (
+                'foo', [('self', None), ('arg1', 'Tuple[int, str]')],
+                [('arg2', 'Optional[int]', 'None')], 'foo docstring', '[\'/index\', \'/foo\']'
+            ),
+            (
+                'bar', [('self', None), ('docs', 'Optional[DocumentArray]')], [('parameters', 'Dict', '{}')],
+                'bar docstring\n:param docs:\n:param parameters:', '\'/bar\''
+            ),
+        ])
+    ),
+    (
+        Path(__file__).parent / 'cases' / 'executor_3',
+        ('Executor3', 'Executor 3', (
+            [('self', None)],
+            [('arg1', 'int', '1'), ('arg2', 'Tuple[str, str]', '(\'123  123\', \'test\\ntest\')')],
+            None
+        ), [
+            ('foo', [('self', None), ('docs', 'DocumentArray')], [('parameters', 'Dict', '{}')], 'foo docstring',
+             '\'/foo\''),
+            ('bar', [('self', None), ('docs', 'DocumentArray')], [('parameters', 'Dict', '{}')], 'bar docstring',
+             'ALL'),
+        ])
+    ),
+])
+def test_get_executor_args(package_path, expected):
+    executor, docstring, init, endpoints, filepath = core.normalize(package_path, meta={'jina': 'master'}, env={})
+    expected_executor, expected_docstring, expected_init, expected_endpoints = expected
+    assert executor == expected_executor
+    assert docstring == expected_docstring
+    assert init == expected_init
+    assert endpoints == expected_endpoints
 
 def test_prelude():
     imports = [
