51eca290e5f655af1fa4a6816e9d2fc2e6285879
mapleeit <522856232@qq.com>
2021-10-20 16:42:11 +0800

    fix(sandbox): endpoints no need to be required
    There is a chance that the executor only have one @requests for all endpoints

--- sandbox/main.py
+++ sandbox/main.py
@@ -3,7 +3,7 @@ from .core import deploy
 
 @click.command()
 @click.option('--executor', type=str, required=True)
-@click.option('--endpoints', type=str, multiple=True, required=True)
+@click.option('--endpoints', type=str, multiple=True)
 @click.option('--replicas', type=int, default=1)
 def main(executor, endpoints, replicas):
     deploy(executor, endpoints, replicas)
