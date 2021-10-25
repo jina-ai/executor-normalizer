299399044e6ea2f8cd702121bfd5140a51a4b1a2
mapleeit <522856232@qq.com>
2021-10-25 16:59:13 +0800

    feat(sandbox): update batch of name

--- sandbox/core.py
+++ sandbox/core.py
@@ -31,17 +31,17 @@ def delete_flow(namespace):
     wait_for_namespace_to_be_deleted(namespace)
 
 
-def create_ingress(executor_lowercase):
-    for service_name, purpose, port in [('gateway', 'flow', '8080'), (executor_lowercase, 'pod', '8081')]:
+def create_ingress(name, namespace):
+    for service_name, purpose, port in [('gateway', 'flow', '8080'), (name, 'pod', '8081')]:
         print(f'create ingress on {service_name} for {purpose}')
-        ingress_name = f'{executor_lowercase}-{purpose}'
+        ingress_name = f'{name}-{purpose}'
         ingress_yaml = yaml.safe_load(
             f'''
 apiVersion: networking.k8s.io/v1beta1\n
 kind: Ingress\n
 metadata:\n
   name: {ingress_name}\n
-  namespace: {executor_lowercase}\n
+  namespace: {namespace}\n
   annotations:\n
     kubernetes.io/ingress.class: nginx\n
     nginx.ingress.kubernetes.io/rewrite-target: /$1\n
@@ -49,27 +49,27 @@ spec:\n
   rules:\n
   - http:\n
       paths:\n
-      - path: /sandbox/{purpose}/{executor_lowercase}/(.+)\n
+      - path: /sandbox/{purpose}/{name}/(.+)\n
         pathType: Prefix\n
         backend:\n
           serviceName: {service_name}\n
           servicePort: {port}\n
         '''
         )
-        networking_api.create_namespaced_ingress(executor_lowercase, ingress_yaml)
-        wait_for_ingress_to_start(ingress_name, executor_lowercase)
+        networking_api.create_namespaced_ingress(namespace, ingress_yaml)
+        wait_for_ingress_to_start(ingress_name, namespace)
 
 
-def wait_for_namespace_to_be_deleted(executor_lowercase):
+def wait_for_namespace_to_be_deleted(namespace):
     for i in range(100):
         namespaces = v1_api.list_namespace()
         namespaces = [item.metadata.name for item in namespaces.items]
-        if executor_lowercase not in namespaces:
+        if namespace not in namespaces:
             print('Namespace is not there. Ready for deployment')
             return
-        print(f'wait for namespace {executor_lowercase} to be deleted')
+        print(f'wait for namespace {namespace} to be deleted')
         time.sleep(5)
-    raise Exception(f'Namespace {executor_lowercase} must be removed before deployment')
+    raise Exception(f'Namespace {namespace} must be removed before deployment')
 
 
 def wait_for_ingress_to_start(ingress_name, namespace):
@@ -82,34 +82,38 @@ def wait_for_ingress_to_start(ingress_name, namespace):
     raise Exception(f'Ingress {ingress_name} in namespace {namespace} can not reach service')
 
 
-def deploy_flow(executor, executor_lowercase, endpoints, replicas):
+def deploy_flow(executor, name, namespace, endpoints, replicas):
     print('deploy flow for: ', executor)
     f = Flow(
-        name=executor_lowercase,
+        name=namespace,
         port_expose=8080,
         infrastructure='K8S',
         protocol='http',
         replicas=replicas,
     ).add(
         uses=f'jinahub+docker://{executor}',
-        name=executor_lowercase
+        name=name
     )
     for endpoint in endpoints:
         f.expose_endpoint(f'/{endpoint}')
     f.start()
     print('create ingress for: ', executor)
-    create_ingress(executor_lowercase)
+    create_ingress(name, namespace)
 
 
 def deploy(executor, endpoints, replicas):
     start_time = time.time()
-    executor_lowercase = executor.lower()
-    delete_flow(executor_lowercase)
-    deploy_flow(executor, executor_lowercase, endpoints, replicas)
+
+    # name for both deployment and service
+    name = executor.lower().replace('_', '-')
+    namespace = f'sandbox-{name}'
+
+    delete_flow(namespace)
+    deploy_flow(executor, name, namespace, endpoints, replicas)
     print(
         f'Deployment of {executor} successful [endpoints: {endpoints}, replicas{replicas}]. Cost {time.time() - start_time} seconds totally')
 
     return {
-        'flow_url': f'http://{SANDBOX_DOMAIN}/sandbox/flow/{executor_lowercase}',
-        'pod_url': f'http://{SANDBOX_DOMAIN}/sandbox/pod/{executor_lowercase}',
+        'flow_url': f'http://{SANDBOX_DOMAIN}/sandbox/flow/{name}',
+        'pod_url': f'http://{SANDBOX_DOMAIN}/sandbox/pod/{name}',
     }
