61eed112f22d5a0139d5090ac2d52d64f631a17a
Zhaofeng Miao <522856232@qq.com>
2022-04-28 13:33:14 +0800

    fix: yaml return octet-stream

--- generator/core.py
+++ generator/core.py
@@ -17,12 +17,12 @@ def generate(executor: str, type: str):
             shutil.make_archive(temp_file_path, 'zip', tmpdirname)
             os.remove(temp_file_path)
         
-        return f'{temp_file_path}.zip'
+        return (f'{temp_file_path}.zip', 'zip')
 
     if type == 'docker_compose':
         f.to_docker_compose_yaml(temp_file_path)
 
-        return temp_file_path
+        return (temp_file_path, 'yaml')
     
     if type == 'jcloud':
         with open(temp_file_path, 'w', encoding='utf-8') as f:
@@ -32,7 +32,7 @@ executors:
   - name: {executor}
     uses: jinahub+docker://{executor}'''
             )
-        return temp_file_path
+        return (temp_file_path, 'yaml')
 
 def clean(path: str):
     logger.info(f'Clean temp file: {path}')
--- server/routes/generator.py
+++ server/routes/generator.py
@@ -10,7 +10,7 @@ from generator.core import generate as generate_yaml, clean as clean_yaml
 
 router = APIRouter()
 
-@router.post('/generate', response_class=FileResponse)
+@router.post('/generate')
 async def generate(
     request: Request,
     block_data: PackagePayload,
@@ -18,7 +18,7 @@ async def generate(
 ):
     now = datetime.datetime.now()
 
-    path = generate_yaml(block_data.executor, block_data.type)
+    (path, file_type) = generate_yaml(block_data.executor, block_data.type)
     # Remove the file after the request is done
     background_tasks.add_task(clean_yaml, path)
 
@@ -29,4 +29,4 @@ async def generate(
         }
     )
 
-    return path
+    return FileResponse(path, media_type='application/octet-stream', filename=f'{block_data.type}.{file_type}')
