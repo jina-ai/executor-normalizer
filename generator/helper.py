3c6517864c582f8e16a750f03b391c83905bb25d
floralatin <dragondao@hotmail.com>
2022-07-01 15:21:40 +0800

    fix: fix func to_j_cloud_yaml

--- generator/helper.py
+++ generator/helper.py
@@ -5,10 +5,13 @@ def to_j_cloud_yaml( output_path: str, executor: str,protocol: str):
     with:
         protocol: {protocol}
     executors:
-      - name: {executor.replace('/','_').replace('-','_')}
+      - name: { executor if executor.isidentifier() else fix_executor_name(executor) }
         uses: jinahub+docker://{executor}'''
     # for safe, don't trust any params
     jsonYaml = yaml.safe_load(document);
     with open(output_path, 'w+', encoding='utf-8') as fs:
         yaml.dump(jsonYaml, fs,sort_keys=False)
     print(f'[b]{output_path}[/b]. You can use it by running')
+
+def fix_executor_name(executor: str):
+    return "default_executor_name"
