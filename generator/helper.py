d1548269890b3eb0e2c5f9926dfba38d04331437
floralatin <dragondao@hotmail.com>
2022-07-03 13:30:03 +0800

    fix: fix func fix_executor_name

--- generator/helper.py
+++ generator/helper.py
@@ -1,3 +1,5 @@
+import re
+
 def to_jcloud_yaml(output_path: str, executor: str,protocol: str):
     import yaml
     document = f'''
@@ -14,4 +16,6 @@ def to_jcloud_yaml(output_path: str, executor: str,protocol: str):
     print(f'[b]{output_path}[/b]. You can use it by running')
 
 def fix_executor_name(executor: str):
-    return "default_executor_name"
+    temp_executor_name = re.sub(r'\W','_',executor);
+    finally_executor_name =  f'_{temp_executor_name}' if re.match(r'^\d',temp_executor_name) else temp_executor_name
+    return finally_executor_name if finally_executor_name.isidentifier() else 'default_executor_name'
