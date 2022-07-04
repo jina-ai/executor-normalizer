66a83fa6e6c6008739429729cf6cbf5acb3858aa
floralatin <dragondao@hotmail.com>
2022-07-04 17:15:27 +0800

    refactor: optimize code

--- generator/helper.py
+++ generator/helper.py
@@ -1,6 +1,8 @@
+import re
+import uuid
+import yaml
 
 def to_jcloud_yaml(output_path: str, executor: str,protocol: str):
-    import yaml
     document = f'''
     jtype: Flow
     with:
@@ -16,18 +18,15 @@ def to_jcloud_yaml(output_path: str, executor: str,protocol: str):
     return True
 
 def check_executor_name(executor):
-    return (is_python_variable_name(executor) and not is_start_with_name(executor))
+    return is_python_variable_name(executor) and not is_start_with_name(executor)
 
 def is_python_variable_name(executor: str):
     return executor.isidentifier();
 
 def is_start_with_name(executor: str):
-    import re
     return True if re.match(r'^_+\d',executor) is not None else False;
 
 def fix_executor_name(executor: str):
-    import re
-    import uuid
     python_variable_name = executor if is_python_variable_name(executor) else re.sub(r'\W','_',executor);
     extends_executor_name = f'executor{python_variable_name}' if is_start_with_name(python_variable_name) else python_variable_name
     finally_executor_name = extends_executor_name if check_executor_name(extends_executor_name) else f'default_executor_name_{str(uuid.uuid4())[0:7]}'
