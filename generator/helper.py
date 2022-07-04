b95ea15167c32d14cc2a4c6d31fcec5a4670ac72
floralatin <dragondao@hotmail.com>
2022-07-04 13:42:09 +0800

    fix func to_jcloud_yaml and add func check_executor_name

--- generator/helper.py
+++ generator/helper.py
@@ -6,7 +6,7 @@ def to_jcloud_yaml(output_path: str, executor: str,protocol: str):
     with:
         protocol: {protocol}
     executors:
-      - name: { executor if executor.isidentifier() else fix_executor_name(executor) }
+      - name: { executor if check_executor_name(executor) else fix_executor_name(executor) }
         uses: jinahub+docker://{executor}'''
     # for safe, don't trust any params
     jsonYaml = yaml.safe_load(document);
@@ -14,8 +14,20 @@ def to_jcloud_yaml(output_path: str, executor: str,protocol: str):
         yaml.dump(jsonYaml, fd, sort_keys=False)
     print(f'[b]{output_path}[/b]. You can use it by running')
 
+def check_executor_name(executor):
+    return (is_python_variable_name(executor) and not is_start_with_name(executor))
+
+def is_python_variable_name(executor: str):
+    return executor.isidentifier();
+
+def is_start_with_name(executor: str):
+    import re
+    return True if re.match(r'^_+\d',executor) is not None else False;
+
 def fix_executor_name(executor: str):
     import re
-    temp_executor_name = re.sub(r'\W','_',executor);
-    finally_executor_name =  f'executor_{temp_executor_name}' if re.match(r'^\d',temp_executor_name) else temp_executor_name
-    return finally_executor_name if finally_executor_name.isidentifier() else 'default_executor_name'
+    import uuid
+    python_variable_name = executor if is_python_variable_name(executor) else re.sub(r'\W','_',executor);
+    extends_executor_name = f'1executor{python_variable_name}' if is_start_with_name(python_variable_name) else python_variable_name
+    finally_executor_name = extends_executor_name if check_executor_name(extends_executor_name) else f'default_executor_name_{str(uuid.uuid4())[0:7]}'
+    return finally_executor_name if check_executor_name(finally_executor_name) else 'default_executor_name'
