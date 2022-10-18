d25cca6e87fb274fa056f005343e228a863de65d
Zhaofeng Miao <522856232@qq.com>
2022-10-18 11:07:53 +0800

    fix: skip checking base class when providing jtype (#55)

--- tests/cases/executor_5/foo.py
+++ tests/cases/executor_5/foo.py
@@ -1,6 +1,7 @@
-from jina import Executor, requests
+from jina import requests
+from jina import Executor as OtherBaseName
 
-class Executor5(Executor):
+class Executor5(OtherBaseName):
     @requests
     def foo(self):
         pass
